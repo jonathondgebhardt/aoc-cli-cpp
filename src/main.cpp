e2f85895c643f5c7d0d2a2c24081b1eba554324f
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2023-11-04 10:40:25 -0400

    Move argument parsing into a function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,50 @@
-#include <algorithm>
 #include <cstdlib>
 #include <cxxopts.hpp>
-#include <iostream>
-#include <vector>
 
-#include "ArgParser.hpp"
 #include "HtmlContent.hpp"
 #include "HttpsRequest.hpp"
 #include "Printer.hpp"
+
+size_t WIDTH = 120;
+
+cxxopts::ParseResult GetArgs(int argc, char** argv)
+{
+    // Commands:
+    // calendar             Show Advent of Code calendar and stars collected [aliases: c]
+    // download             Save puzzle description and input to files [aliases: d]
+    // read                 Read puzzle statement (the default command) [aliases: r]
+    // submit               Submit puzzle answer [aliases: s]
+    // private-leaderboard  Show the state of a private leaderboard [aliases: p]
+    // help                 Print this message or the help of the given subcommand(s)
+    //
+    // Options:
+    //-d, --day <DAY>            Puzzle day [default: last unlocked day (during Advent of Code
+    //month)] -y, --year <YEAR>          Puzzle year [default: year of current or last Advent of
+    //Code event] -s, --session-file <PATH>  Path to session cookie file [default:
+    //~/.adventofcode.session] -w, --width <WIDTH>        Width at which to wrap output [default:
+    //terminal width] -o, --overwrite            Overwrite files if they already exist -I,
+    //--input-only           Download puzzle input only -P, --puzzle-only          Download puzzle
+    //description only -i, --input-file <PATH>    Path where to save puzzle input [default: input]
+    //-p, --puzzle-file <PATH>   Path where to save puzzle description [default: puzzle.md]
+    //-q, --quiet                Restrict log messages to errors only
+    //--debug                Enable debug logging
+    //-h, --help                 Print help information
+    //-V, --version              Print version information
+
+    // TODO: Add a brief description
+    cxxopts::Options options{"aoc-cli", ""};
+
+    // TODO: Don't blatantly copy+paste from rust aoc-cli :(
+    // clang-format off
+    options.add_options()
+        ("r,read", "Read puzzle statement (the default command)", cxxopts::value<bool>()->default_value("true"))
+        ("y,year", "Puzzle year [default: year of current or last Advent of Code event]", cxxopts::value<std::string>())
+        ("d,day", "Puzzle day [default: last unlocked day (during Advent of Code month)]", cxxopts::value<std::string>())
+        ;
+    // clang-format on
+
+    return options.parse(argc, argv);
+}
 
 void Read(const std::string& year, const std::string& day)
 {
@@ -30,42 +67,9 @@ void Read(const std::string& year, const std::string& day)
     }
 }
 
-// Commands:
-// calendar             Show Advent of Code calendar and stars collected [aliases: c]
-// download             Save puzzle description and input to files [aliases: d]
-// read                 Read puzzle statement (the default command) [aliases: r]
-// submit               Submit puzzle answer [aliases: s]
-// private-leaderboard  Show the state of a private leaderboard [aliases: p]
-// help                 Print this message or the help of the given subcommand(s)
-//
-// Options:
-//-d, --day <DAY>            Puzzle day [default: last unlocked day (during Advent of Code month)]
-//-y, --year <YEAR>          Puzzle year [default: year of current or last Advent of Code event]
-//-s, --session-file <PATH>  Path to session cookie file [default: ~/.adventofcode.session]
-//-w, --width <WIDTH>        Width at which to wrap output [default: terminal width]
-//-o, --overwrite            Overwrite files if they already exist
-//-I, --input-only           Download puzzle input only
-//-P, --puzzle-only          Download puzzle description only
-//-i, --input-file <PATH>    Path where to save puzzle input [default: input]
-//-p, --puzzle-file <PATH>   Path where to save puzzle description [default: puzzle.md]
-//-q, --quiet                Restrict log messages to errors only
-//--debug                Enable debug logging
-//-h, --help                 Print help information
-//-V, --version              Print version information
-
 int main(int argc, char** argv)
 {
-    cxxopts::Options options{"aoc-cli", ""};
-
-    // clang-format off
-    options.add_options()
-        ("r,read", "Read puzzle statement (the default command)", cxxopts::value<bool>()->default_value("true"))
-        ("y,year", "Puzzle year [default: year of current or last Advent of Code event]", cxxopts::value<std::string>())
-        ("d,day", "Puzzle day [default: last unlocked day (during Advent of Code month)]", cxxopts::value<std::string>())
-        ;
-    // clang-format on
-
-    const auto result = options.parse(argc, argv);
+    const auto result = GetArgs(argc, argv);
     const auto year = result["year"].as<std::string>();
     const auto day = result["day"].as<std::string>();
 
